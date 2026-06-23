7ad79ee39470a7ce9c56b2b27535efd44ef59bad
Kevin Meyer <kevin.meyer@webality.de>
2026-06-23 16:12:44 +0200

    database instance

---
+++ src/AppwriteHelper/Services/Database.cs
@@ -0,0 +1,56 @@
+﻿using Appwrite.Services;
+
+namespace AppwriteHelper.Services
+{
+    public abstract class Database
+    {
+        private TablesDB? UserTables;
+        private TablesDB? ServerTables;
+
+        private IAppwriteClientFactory? _userAppwriteClient;
+        private IAppwriteClientFactory? _serverAppwriteClient;
+
+        public void SetUserClientFactory(IAppwriteClientFactory client)
+        {
+            _userAppwriteClient = client;
+        }
+
+        public void SetServerClientFactory(IAppwriteClientFactory client)
+        {
+            _serverAppwriteClient = client;
+        }
+
+        private TablesDB GetOrInitUserTables()
+        {
+            if (UserTables == null)
+            {
+                if (_userAppwriteClient?.Client == null)
+                    throw new InvalidOperationException();
+
+                UserTables = new(_userAppwriteClient.Client);
+            }
+
+            return UserTables;
+        }
+
+        private TablesDB GetOrInitServerTables()
+        {
+            if (ServerTables == null)
+            {
+                if (_serverAppwriteClient?.Client == null)
+                    throw new InvalidOperationException();
+
+                ServerTables = new(_serverAppwriteClient.Client);
+            }
+
+            return ServerTables;
+        }
+
+        protected TablesDB GetTables(bool userServerClient)
+        {
+            if (userServerClient)
+                return GetOrInitServerTables();
+            return GetOrInitUserTables();
+        }
+    }
+}
---
+++ src/AppwriteHelper/Services/DatabaseInstance.cs
@@ -0,0 +1,31 @@
+﻿using Appwrite.Models;
+using Appwrite.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppwriteHelper.Services
+{
+    public class DatabaseInstance : Database, IDatabaseInstance
+    {
+        public DatabaseInstance(
+          [FromKeyedServices(Constants.APPWRITE_CLIENT_USER)] IAppwriteClientFactory? userAppwriteClient = null,
+          [FromKeyedServices(Constants.APPWRITE_CLIENT_SERVER)] IAppwriteClientFactory? serverAppwriteClient = null)
+        {
+            if (userAppwriteClient != null)
+                SetUserClientFactory(userAppwriteClient);
+
+            if (serverAppwriteClient != null)
+                SetServerClientFactory(serverAppwriteClient);
+        }
+
+        public TablesDB GetDatabase(bool useServerClient)
+        {
+            return GetTables(useServerClient);
+        }
+    }
+}
