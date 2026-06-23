983eb4886d79598b121fc8cbcc1ae44fcaacdacb
Kevin Meyer <kevin.meyer@webality.de>
2026-06-23 15:09:53 +0200

    added row methods to interface

--- src/AppwriteHelper/Collections/IGenericCollection.cs
+++ src/AppwriteHelper/Collections/IGenericCollection.cs
@@ -10,5 +10,13 @@
         Task<T?> UpdateDocument(T document, List<string>? permissions = null, bool useServerClient = false);
         Task<T?> UpdateDocument(string documentId, object? document, List<string>? permissions = null, bool useServerClient = false);
         Task<T?> CreateDocument(T document, List<string>? permissions = null, bool useServerClient = false);
+        Task<T?> GetRow(string id, bool useServerClient = false, string? transactionId = null);
+        Task<IEnumerable<T>> GetRows(List<string>? queries = null, bool useServerClient = false, string? transactionId = null);
+        Task<T?> UpsertRow(T row, List<string>? permissions = null, bool useServerClient = false, string? transactionId = null);
+        Task<T?> UpsertRow(string rowId, object? data, List<string>? permissions = null, bool useServerClient = false, string? transactionId = null);
+        Task<T?> UpdateRow(T row, List<string>? permissions = null, bool useServerClient = false, string? transactionId = null);
+        Task<T?> UpdateRow(string rowId, object? data, List<string>? permissions = null, bool useServerClient = false, string? transactionId = null);
+        Task<T?> CreateRow(T document, List<string>? permissions = null, bool useServerClient = false, string? transactionId = null);
+        Task<object?> DeleteDocument(string rowId, bool useServerClient = false, string? transactionId = null);
     }
 }
